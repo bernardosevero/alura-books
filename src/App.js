7cb06596da720e86457677a820deeb177da57218
Bernardo Severo <bernardo.severo@sap.com>
2022-05-31 09:42:26 -0300

    aula 5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 import HeaderMenu from './components/HeaderMenu/HeaderMenu'
+import NewReleases from './components/NewReleases/NewReleases'
+import Search from './components/Search/Search'
 
 const AppWrapper = styled.body`
-  background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
   height: 100vh;
   width: 100vw;
 `
@@ -11,6 +12,8 @@ function App() {
   return (
       <AppWrapper>
         <HeaderMenu />
+        <Search />
+        <NewReleases />
       </AppWrapper>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  *  {
+    margin: 0;
+    font-family:'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    font-family:'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
